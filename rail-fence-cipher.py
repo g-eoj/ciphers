2e17b82f3c1e498c960a0ad2241e5bed80084a42
Joe G <joe.granados@hotmail.com>
2013-06-06 12:38:26 -0700

    Add and organize tests

--- rail-fence-cipher.py
+++ rail-fence-cipher.py
@@ -49,18 +49,45 @@ def decipher(cipher_text, rails):
         chunks[rail_p] = chunks[rail_p].replace(chunks[rail_p][0], '', 1)
     return plain_text.lower()
 
-test_cases = [("What's going on?", 3), ("What's going on?", 4), ("Ordinal: measures by rank order only.", 6),
-              ("The ink drawings, of course, will be jeered at as obvious impostures;\n notwithstanding a strangeness of technique which art experts ought to remark and puzzle over.", 11)]
-for t in test_cases:
-    e = encipher(t[0], t[1])
-    d = decipher(e, t[1])
-    if alphaNumFilter(t[0]).lower() == d:
-        print "Pass:", t
-        print "Encipher:", e
-        print "Decipher:", d
-        print
+
+# TESTS
+import time
+
+def timedcall(fn, *args):
+    "Call function with args; return the time in second and result."
+    t0 = time.clock()
+    g = fn(*args)
+    result = [next(g) for i in xrange(10 ** 3)]
+    t1 = time.clock()
+    return t1 - t0, result[:10]
+
+
+def average_time(n, fn, *args):
+    if isinstance(n, int):
+        times = [timedcall(fn, *args)[0] for i in xrange(n)]
     else:
-        print "Fail:", t
-        print "Encipher:", e
-        print "Decipher:", d
-        print
+        times = []
+        while sum(times) < n:
+            times.append(timedcall(fn, *args)[0])
+    return sum(times) / len(times)
+
+
+def run_tests():
+    test_cases = [("What's going on?", 3), ("What's going on?", 4), ("Ordinal: measures by rank order only.", 6),
+              ("The ink drawings, of course, will be jeered at as obvious impostures;\n notwithstanding a strangeness of technique which art experts ought to remark and puzzle over.", 11)]
+    for t in test_cases:
+        e = encipher(t[0], t[1])
+        d = decipher(e, t[1])
+        if alphaNumFilter(t[0]).lower() == d:
+            print "Pass:", t
+            print "Encipher:", e
+            print "Decipher:", d
+            print
+        else:
+            print "Fail:", t
+            print "Encipher:", e
+            print "Decipher:", d
+            print
+
+    # print timedcall(railPosition, 7)
+    # print average_time(10.0, railPosition, 7)
